49f2fa19b611ba47fdabe49a1f378340aeb8601d
paninapress <NPacifico25@gmail.com>
2014-03-18 13:50:25 -0700

    added secret_key_base

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-SlowestLab::Application.config.secret_key_base = '21d1e9f4e4784e4a4dcdede21d593e7adf3907bb57566d88b7a8b9c123d035ea9d3bdc8b53e89af9be16a0ec779afadf5f2e523715e931c22e9101358d98dccf'
+SlowestLab::Application.config.secret_key_base = 'SECRET_KEY_BASE'
